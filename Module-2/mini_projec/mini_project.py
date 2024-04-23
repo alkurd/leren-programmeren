29ea88af6b6aabfdcdd17c8faac7a1ed0af36df9
alkurd <yalkerdi@gmail.com>
2024-04-23 09:31:00 +0200

    update

--- Module-2/mini_projec/mini_project.py
+++ Module-2/mini_projec/mini_project.py
@@ -1,25 +1,27 @@
-import random
-namen = []
+# import random
+# namen = []
 
-while True:
-    naam = input('Geef een naam: ')
-    if naam in namen:
-        print(f'De naam {naam} is al in de lijst!')
-    else:
-        namen.append(naam)
-    if len(namen) >= 3:
-        naam = input("Wil je nog een naam invoeren (I) of lootjes trekken (L)? ").upper()
-        if naam == 'L':
-            break
+# while True:
+#     naam = input('Geef een naam: ')
+#     if naam in namen:
+#         print(f'De naam {naam} is al in de lijst!')
+#     else:
+#         namen.append(naam)
+#     if len(namen) >= 3:
+#         naam = input("Wil je nog een naam invoeren (I) of lootjes trekken (L)? ")
+#         if naam == 'l':
+#             break
 
-nuw_list = namen.copy()
-random.shuffle(nuw_list)
+# nuw_list = namen.copy()
+# random.shuffle(nuw_list)
 
-for i in range(len(namen)):
-    while nuw_list[i] == namen[i]:
-        random.shuffle(nuw_list)
-    if nuw_list[i] != namen[i]:
-        print(f"{namen[i]} trekt {nuw_list[i]}")
+# for i in range(len(namen)):
+#     while nuw_list[i] == namen[i]:
+#         random.shuffle(nuw_list)
+#     if nuw_list[i] != namen[i]:
+#         print(f"{namen[i]} trekt {nuw_list[i]}")
+   
 
 
- 
+x = 6 / 2
+print (x)
