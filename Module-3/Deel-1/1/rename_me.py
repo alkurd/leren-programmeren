78259f58746ee0584cd6b05e916b5e4a5b192e16
alkurd <yalkerdi@gmail.com>
2024-05-15 12:36:22 +0200

    update
    Hernoem (refactor) alles naar logische namen

--- Module-3/Deel-1/1/rename_me.py
+++ Module-3/Deel-1/1/rename_me.py
@@ -1,67 +1,57 @@
-def quantum_broodrooster(stellar_broccoli:int) -> bool:
-    return stellar_broccoli % 2 == 0
+def is_even(number: int) -> bool:
+    return number % 2 == 0
 
 # deze functie neemt de parameter als een invoer en rtuneert een boeling (True ale het even is en False ales eht oneven is )
 
-resultaat = quantum_broodrooster(int(input('geef en getaal: ')))
+resultaat = is_even(int(input('geef en getaal: ')))
 print(resultaat)
 
-def chaos_papegaai(fantasie_platypus:str) -> str:
-    betoverde_druif = fantasie_platypus.split()
-    doldwaze_broccoli = betoverde_druif[::-1]
-    tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
-    return tijdmachine_pannenkoekenmix
+def revers_woords(string:str) -> str:
+    worden = string.split()
+    reverd_woords = worden[::-1]
+    reversd_string = ' '.join(reverd_woords)
+    return reversd_string
 
-# deze functie draait de string worden agterste voren
+# deze functie draait de string worden achterste voren
 
-print(chaos_papegaai("hallo yousef ik yous.")) 
+print(revers_woords("yousef alkurdi is 23 jaar oud.")) 
 
-def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
-    planetair_taartje = set(galactische_snoepjes)
-    whatchamacallit = len(planetair_taartje)
-    return whatchamacallit
+def unique_chars_ounter(sentence:str) -> int:
+    unique_chars = set(sentence)
+    character_counter = len(unique_chars)
+    return character_counter
 
-# deze functie telt de aantal characters van de string's         
+# Deze functie telt de aantal unieke karakters in de string en geeft de aantal terug (Uneke karakters zijn die letters die niet meer herhaald worden)       
                    
-resultaat = kosmische_koekjesmix('input_string - input string')
+resultaat = unique_chars_ounter('hello world')
 print("Het aantal unieke karakters is:", resultaat)  
 
-def ruimte_hamsterwiel(planetair_taartje:str) -> float:
-    wobbelwobbel = planetair_taartje.split()
+def gemiddelde_woordlengte(zin: str) -> float:
+    woorden = zin.split()
     
-    blork = 0
-    for snorkelwagen in wobbelwobbel:
-        blork += len(snorkelwagen)
+    totale_lengte = 0
+    for woord in woorden:
+        totale_lengte += len(woord)
 
-    bizarro_matrix = blork / len(wobbelwobbel)
-    return bizarro_matrix
+    gemiddelde_woordlengte = totale_lengte / len(woorden)
+    return gemiddelde_woordlengte
 
 # deze functie verandert een string in een list en telt de characters van elke item in de list en telt ze op en daarna deelt ze door de aantal items in de list
 
-print(ruimte_hamsterwiel("hallo.yousef yousef"))
+print(gemiddelde_woordlengte("hallo.yousef yousef"))
 
+def print_tafels(getal: int, aantal_keer: int = 10) -> None:
+    for teller in range(1, aantal_keer + 1):
+        product = teller * getal
+        print(f'{teller} x {getal} = {product}')
 
-
-def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
-    for zwabber_krakeling in range(1, parallelle_tosti+1):
-        laser_sandwich = zwabber_krakeling * infinity_pizza
-        print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
 # De functie spaghetti_spaceship genereert en drukt een tafel van vermenigvuldiging af voor een specifiek getal (infinity_pizza). Deze tafel wordt een aantal keer afgedrukt dat wordt bepaald door het argument parallelle_tosti
 #in het gevaal dat je de aantal vermenigvuldiging is niet aan ge geven word de aan gegeven getaal vermenigvuldigt met 10
 
-
-spaghetti_spaceship(5,5)
-
-
-# yousef = 'yousef alkurdi is 23 jaar oud'.split()
-# a = 0 
-# for i in reversed(yousef):
-#     a += len(i)
-# b = a / len(i)
+print_tafels(5,5)
 
 
 
-# print(len(yousef))
-# print(a)
-# print(b)
-# print(yousef)
+
+
+
