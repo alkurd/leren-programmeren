2c38e9b867a115c4e46723785de5f0eb2cc9d3a2
alkurd <yalkerdi@gmail.com>
2024-05-24 11:05:27 +0200

    uodate

--- Module-3/Deel-1/5/NaamLeefTijd.py
+++ Module-3/Deel-1/5/NaamLeefTijd.py
@@ -4,7 +4,20 @@ def NameAndAge():
     library = {'name': name, 'age': age}
     return library
 
-persoon = NameAndAge()
 
-print(f"{persoon['name']} is {persoon['age']} jaar")
 
+def getMultipleNamesAndAges():
+    lijst = []
+    while True:
+        naam = NameAndAge()
+        lijst.append(naam)
+        NieuwNaam = input('wil je nog een naam toevoegen?' ).lower()
+        if NieuwNaam != 'ja':
+            break
+    return lijst
+
+
+people = getMultipleNamesAndAges()
+
+for person in people:
+    print(f"{person['name']} is {person['age']} jaar")
