e3253298e2e54185cd340938e6390f2a9936f6c8
alkurd <yalkerdi@gmail.com>
2024-05-28 11:09:22 +0200

    rekenmachine

--- Module-3/Deel-1/10/functions.py
+++ Module-3/Deel-1/10/functions.py
@@ -1,44 +1,59 @@
+#1
 def addition(num1: int, num2: int):
     return num1 + num2
 
+#2
 def subtraction(num1: int, num2: int):
     return num1 - num2
 
+#3
 def multiplication(num1: int, num2: int):
     return num1 * num2
 
+#4
 def division(num1: int, num2: int):
     if num2 != 0:
         return num1 / num2
     else:
         return "Delen door nul is niet toegestaan"
 ###########################
-def increment(num1: int):
-    return num1 + 1
+#5
+# def increment(num1: int):
+#     return num1 + 1
 
-def decrement(num1: int):
-    return num1 - 1
+# #6
+# def decrement(num1: int):
+#     return num1 - 1
 
-def double(num1: int):
-    return num1 * 2
+# #7
+# def double(num1: int):
+#     return num1 * 2
 
-def halve(num1: int):
-    return num1 / 2
+# #8
+# def halve(num1: int):
+#     return num1 / 2
 ###########################
+#9
 def square(num1:int):
     return num1 ** 2
 
+#10
 def cube(num1:int):
     return num1 ** 3
 
+#11
 def square_root(num1:int):
     return num1 ** (1/2)
 
+#12
 def cube_root(num1:int):
     return num1 ** (1/3)
 
+#13
 def factorial(num1:int):
     if num1 == 0:
         return 1
     else:
         return num1 * factorial(num1 - 1)
+
+
--- Module-3/Deel-1/10/rekenmachine.py
+++ Module-3/Deel-1/10/rekenmachine.py
@@ -1,208 +1,132 @@
-from functions import addition, subtraction, multiplication, division, increment, decrement, double, halve, square, cube, square_root, cube_root, factorial
-
-def perform_operation(choice, a, b=None):
-    if choice == "A":
-        result = addition(a, b)
-        print(f"\n{a} + {b} = {result}\n")
+from functions import addition, subtraction, multiplication, division
+from termcolor import colored
+def perform_operation(choice, n1, n2=None):
+    if choice in ['A','E']:
+        result = addition(n1, n2)
+        print(f"\n{n1} + {n2} = {result}\n")
         return result
-    elif choice == "B":
-        result = subtraction(a, b)
-        print(f"\n{a} - {b} = {result}\n")
+    elif choice in ['B','F']:
+        result = subtraction(n1, n2)
+        print(f"\n{n1} - {n2} = {result}\n")
         return result
-    elif choice == "C":
-        result = multiplication(a, b)
-        print(f"\n{a} * {b} = {result}\n")
+    elif choice in ['C', 'G']:
+        result = multiplication(n1, n2)
+        print(f"\n{n1} * {n2} = {result}\n")
         return result
-    elif choice == "D":
-        result = division(a, b)
-        if isinstance(result, str):
-            print(result)
-        else:
-            print(f"\n{a} / {b} = {result}\n")
-        return result
-    elif choice == "E":
-        result = increment(a)
-        print(f"\n{a} + 1 = {result}\n")
-        return result
-    elif choice == "F":
-        result = decrement(a)
-        print(f"\n{a} - 1 = {result}\n")
-        return result
-    elif choice == "G":
-        result = double(a)
-        print(f"\n{a} * 2 = {result}\n")
-        return result
-    elif choice == "H":
-        result = halve(a)
-        print(f"\n{a} / 2 = {result}\n")
-        return result
-    elif choice == 'I':
-        result = square(a)
-        print(f"\n{a} ^ 2 = {result}\n")
-        return result
-    elif choice == 'J':
-        result = cube(a)
-        print(f"\n{a} ^ 3 = {result}\n")
-        return result
-    elif choice == 'K':
-        result = square_root(a)
-        print(f"\n√{a} = {result}\n")
-        return result
-    elif choice == 'L':
-        result = cube_root(a)
-        print(f"\n∛{a} = {result}\n")
-        return result
-    elif choice == 'M':
-        result = factorial(a)
-        print(f"\n{a}! = {result}\n")
-        return result
-# StopWorden = ["stop", "nee", "niet", "genoeg", "klaar", "exit", "afsluiten"]
+    elif choice in['D','H']:
+            result = division(n1, n2)
+            print(f"{n1} / {n2} = {result}")
+            return result
+keuzen_lijst = ["Wat wilt u doen? Kies een van de volgende opties:",
+                "A) getallen optellen",
+                "B) getallen aftrekken",
+                "C) getallen vermenigvuldigen",
+                "D) getallen delen",
+                "E) getal ophogen",
+                "F) getal verlagen",
+                "G) getal verdubbelen",
+                "H) getal halveren",
+                "Uw keuze: "]
+letter = ['A','B','C','D','E','F','G','H']
+firstRound = True
+n1 = None
 while True:
-    choice = input("Wat wilt u doen? Kies een van de volgende opties:\n"
-                "A) getallen optellen\n"
-                "B) getallen aftrekken\n"
-                "C) getallen vermenigvuldigen\n"
-                "D) getallen delen\n"
-                "E) getal ophogen\n"
-                "F) getal verlagen\n"
-                "G) getal verdubbelen\n"
-                "H) getal halveren\n"
-                "I) getal kwadraat\n"
-                "J) getal kubus\n"
-                "K) vierkantswortel\n"
-                "L) derdemachtswortel\n"
-                "M) faculteit\n"
-                "S) stopen\n"
-                "Uw keuze: ").upper()
+        if not firstRound:
+            keuzen_lijst[0] = f'Wat wil je met ({colored(n1,'red')}) doen\n'
+        choice = input('\n'.join(keuzen_lijst)).upper()
+        if choice in letter:
+            firstRound = False
+            if 'S' not in letter:
+                letter.append('S')
+                keuzen_lijst.insert(-1, 'S) Stoppen')
+            if n1 == None:
+                n1 = int(input("Geef uw eerste getal: "))
+            if choice in ['E','F','G','H']:
+                n2 =  1 if choice in ['E','F'] else 2
+            elif choice == 'S':
+                print("Programma beëindigd.")
+                break
+            else:
+                n2 = int(input("Geef uw tweede getal: "))
+            result = perform_operation(choice,n1,n2)
+            n1 = result
+        else:
+            print("Ongeldige keuze! Probeer opnieuw.")
 
-    if choice in ['A', 'B', 'C', 'D']:
-            a = float(input("Voer het eerste getal in: "))
-            b = float(input("Voer het tweede getal in: "))
-            result = perform_operation(choice, a, b)
-    elif choice == 'S':
-         print("Bedankt voor het gebruiken van de calculator. Tot ziens!")
-         exit()
-    elif choice in ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']:
-            a = float(input("Voer het getal in: "))
-            result = perform_operation(choice, a)
-    # print()
-    # print("--------------------------------")
-    # print(f"Het resultaat is: {result}")
-    # print("--------------------------------")
-    # print()
-    while True:
-        next_choice = input(f'Wil je wat met {result} doen?\n'
-                            'A) Iets optellen\n'
-                            'B) Iets aftrekken\n'
-                            'C) Iets vermenigvuldigen\n'
-                            'D) Ergens door delen\n'
-                            'E) Ophogen\n'
-                            'F) Verlagen\n'
-                            'G) Verdubbelen\n'
-                            'H) Halveren\n'
-                            'I) Kwadraat\n'
-                            'J) Kubus\n'
-                            'K) Vierkantswortel\n'
-                            'L) Derdemachtswortel\n'
-                            'M) Faculteit\n'
-                            'N) Nieuwe getallen invoeren\n'
-                            "S) stopen\n"
-                            'Uw keuze: ').upper()
-        if next_choice in ['A', 'B', 'C', 'D']:
-                b = float(input("Voer het tweede getal in: "))
-                result = perform_operation(next_choice, result, b)
-        elif next_choice in ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']:
-                result = perform_operation(next_choice, result)
-        elif next_choice =='N':
-            break
-        elif next_choice == 'S':
-            print("Bedankt voor het gebruiken van de calculator. Tot ziens!")
-            exit()
-    print()
-    print("--------------------------------")
-    print(f"Het resultaat is: {result}")
-    print("--------------------------------")
-    print()
+    
+        
+    
 
 #############################################################################################
     
 
 
-# # rekenmachine.py
+# rekenmachine.py
+
 # from functions import addition, subtraction, multiplication, division
 
-# def get_number_input(prompt):
-#     return float(input(prompt))
+
 
 # def main():
 #     firstRound = True
 #     n1 = False
-
+#     voorbeeld = ['Wat wilt u doen?',
+#                 'A) getallen optellen',
+#                 'B) getallen aftrekken',
+#                 'C) getallen vermenigvuldigen',
+#                 'D) getallen delen',
+#                 'E) getal ophogen',
+#                 'F) getal verlagen',
+#                 'G) getal verdubbelen',
+#                 'H) getal halveren'
+#                 ]
+#     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 #     while True:
-#         if firstRound:
-#             choice = input('Wat wilt u doen?\n'
-#                            'A) getallen optellen\n'
-#                            'B) getallen aftrekken\n'
-#                            'C) getallen vermenigvuldigen\n'
-#                            'D) getallen delen\n'
-#                            'E) getal ophogen\n'
-#                            'F) getal verlagen\n'
-#                            'G) getal verdubbelen\n'
-#                            'H) getal halveren\n')
-                            
-#             firstRound = False
-#         else:
-#             choice = input(f"Wil je wat met de uitkomst ({uitkomst}) doen?\n"
-#                            'A) iets optellen\n'
-#                            'B) iets aftrekken\n'
-#                            'C) met iets vermenigvuldigen\n'
-#                            'D) ergens door delen\n'
-#                            'E) ophogen\n'
-#                            'F) verlagen\n'
-#                            'G) verdubbelen\n'
-#                            'H) halveren\n'
-#                            'I) niets?\n')
+#         while True:
+#             choice = input('\n'.join(voorbeeld)+'\n').upper()  
+#             if choice in letters:
+#                 break
+#             else:
+#                 print("Ongeldige keuze! Probeer opnieuw.")
 
-#         if choice.upper() == 'I':
+#         if firstRound:
+#             voorbeeld.append ('I) niets?') 
+#             letters.append ('I')
+
+#             # voorbeeld[0] = f'Wil je wat met de uitkomst ({n1}) doen?'
+#             firstRound = False
+#             n1 = float(input("Voer het eerste getal in: "))
+#         # else:
+#         #     choice = input(voorbeeld).upper()
+
+#         if choice == 'I':
 #             break
         
-#         if n1 == False:
-#             n1 = get_number_input("Voer het eerste getal in: ")
+        
 
-#         if choice.upper() in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
-#             if choice.upper() in ['E', 'F', 'G', 'H']:
-#                 n2 = 1 if choice.upper() in ['E', 'F'] else 2
-#             else:
-#                 n2 = get_number_input("Voer het tweede getal in: ")
-
-#         if choice.upper() == 'A':
-#             uitkomst = addition(n1, n2)
-#             print(f"{n1} + {n2} = {uitkomst}")
-#         elif choice.upper() == 'B':
-#             uitkomst = subtraction(n1, n2)
-#             print(f"{n1} - {n2} = {uitkomst}")
-#         elif choice.upper() == 'C':
-#             uitkomst = multiplication(n1, n2)
-#             print(f"{n1} * {n2} = {uitkomst}")
-#         elif choice.upper() == 'D':
-#             uitkomst = division(n1, n2)
-#             print(f"{n1} / {n2} = {uitkomst}")
-#         elif choice.upper() == 'E':
-#             uitkomst = addition(n1, n2)
-#             print(f"{n1} + {n2} = {uitkomst}")
-#         elif choice.upper() == 'F':
-#             uitkomst = subtraction(n1, n2)
-#             print(f"{n1} - {n2} = {uitkomst}")
-#         elif choice.upper() == 'G':
-#             uitkomst = multiplication(n1, n2)
-#             print(f"{n1} * {n2} = {uitkomst}")
-#         elif choice.upper() == 'H':
-#             uitkomst = division(n1, n2)
-#             print(f"{n1} / {n2} = {uitkomst}")
+#         if choice in ['E', 'F', 'G', 'H']:
+#             n2 = 1 if choice in ['E', 'F'] else 2
 #         else:
-#             print("Ongeldige keuze! Probeer opnieuw.")
+#             n2 = float(input("Voer het tweede getal in: "))
+
+
+#         if choice in['A','E']:
+#             uitkomst = addition(n1, n2)
+#             print(f"{n1} + {n2} = {uitkomst}")
+#         elif choice in['B','F']:
+#             uitkomst = subtraction(n1, n2)
+#             print(f"{n1} - {n2} = {uitkomst}")
+#         elif choice in['C','G']:
+#             uitkomst = multiplication(n1, n2)
+#             print(f"{n1} * {n2} = {uitkomst}")
+#         elif choice in['D','H']:
+#             uitkomst = division(n1, n2)
+#             print(f"{n1} / {n2} = {uitkomst}")
+        
 
 #         n1 = uitkomst
+#         voorbeeld[0] = f'Wil je wat met de uitkomst ({n1}) doen?'
+        
 
 # if __name__ == "__main__":
 #     main()
